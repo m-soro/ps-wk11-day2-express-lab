88e71325f38ed01ea1d145bc5a4f9a2ff2101dbb
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-15 14:41:52 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/greeting", (req, res) => {
  */
 app.get("/greeting/:name", (req, res) => {
   res.send(
-    `<h2>Hey ${req.params.name.toLocaleUpperCase()}, nice to see you!<h2>`
+    `<h2>Hey ${req.params.name.toLocaleUpperCase()}, nice to see you!</h2>`
   );
 });
 /**
@@ -32,8 +32,6 @@ app.listen(3001, () =>
 
 /**
  * TIP CALCULATOR
- */
-/**
  * (2) Your app should have a route of '/tip'and it should expect 2 params. One should be totaland one should be tipPercentage.
  * (3)When hitting the route, the page should display how much your tip will be based on the total amount of the bill and the tip percentage. (ex. hitting '/tip/100/20'should display 20on the page).
  */
@@ -41,5 +39,47 @@ app.listen(3001, () =>
 app.get("/tip/:bill/:tipPercentage", (req, res) => {
   const bill = req.params.bill;
   const tipPercentage = req.params.tipPercentage;
-  res.send(`<h2>You should tip ${(tipPercentage / bill) * 100}<h2>`);
+  res.send(`<h2>You should tip ${(tipPercentage / bill) * 100}</h2>`);
+});
+
+/**
+ * MAGIC 8 BALL
+ * (2) Create a route of '/magic'that should expect a phrase in the URL that ask the Magic 8 ball a question.
+(3) So if the user hits that route and asks a question of "Will I be a Millionaire" (ex. '/magic/Will%20I%20Be%20A%20Millionaire') he should have return to him his question AND a random Magic 8 ball response (see the array below) on the screen.
+(4) We can't use spaces in the url, so we use %20to express a space in the url.
+(5) So if the user hits that route and asks a question of "Will I be a Millionaire" he should get his question asked and a random Magic 8 ball quote on the screen.
+(6) Send the magic 8 ball response back between html <h1>tags
+(7) Use this array of Magic 8 ball responses.....
+
+["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+ */
+
+app.get("/magic/:question", (req, res) => {
+  const magic8ball = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  let magicResponse = magic8ball[Math.floor(Math.random() * magic8ball.length)];
+  const question = req.params.question;
+  res.send(`<h1>You asked: ${question}</h1>
+    <h2>Magic 8 ball says:${magicResponse}</h2>
+  `);
 });
