b6aa343460e1a75befd313452cb373f54f049eb9
Mark Soro <napoleon_soro@rocketmail.com>
2023-09-15 14:29:31 -0400

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 /**
  * GREETINGS
+ * ---------
  * (2) Make a route '/greeting'that sends a generic greeting to the screen like "Hello, stranger".
  */
 app.get("/greeting", (req, res) => {
@@ -14,17 +15,13 @@ app.get("/greeting", (req, res) => {
 });
 /**
  * (3) Give the greetingroute a param /:name
+ * (4) When hitting the route, the page should display a message such as "Hello, ", or "What's up, <name>", * or "<name>! It's so great to see you!" (ex. hitting '/greeting/Jimmy-boy'should display Wow! Hello * * there, Jimmy-boyon the page).
  */
 app.get("/greeting/:name", (req, res) => {
   res.send(
     `<h2>Hey ${req.params.name.toLocaleUpperCase()}, nice to see you!<h2>`
   );
 });
-
-/**
- * (4) When hitting the route, the page should display a message such as "Hello, ", or "What's up, <name>", * or "<name>! It's so great to see you!" (ex. hitting '/greeting/Jimmy-boy'should display Wow! Hello * * there, Jimmy-boyon the page).
- */
-
 /**
  * PART OF STEP (1)
  * Tell the server where to listen for requests (the port).
@@ -32,3 +29,17 @@ app.get("/greeting/:name", (req, res) => {
 app.listen(3001, () =>
   console.log("This is Express App Listening to port 3001!")
 );
+
+/**
+ * TIP CALCULATOR
+ */
+/**
+ * (2) Your app should have a route of '/tip'and it should expect 2 params. One should be totaland one should be tipPercentage.
+ * (3)When hitting the route, the page should display how much your tip will be based on the total amount of the bill and the tip percentage. (ex. hitting '/tip/100/20'should display 20on the page).
+ */
+
+app.get("/tip/:bill/:tipPercentage", (req, res) => {
+  const bill = req.params.bill;
+  const tipPercentage = req.params.tipPercentage;
+  res.send(`<h2>You should tip ${(tipPercentage / bill) * 100}<h2>`);
+});
